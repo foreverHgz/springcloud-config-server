d5afa828defe289e46b48fdc85e19c9261d0af0b
gzhuang <forever_hgz@sina.com>
2024-04-30 16:25:31 +0800

    初始化spring cloud config信息

--- src/main/java/com/springcloud/config/SpringCloudConfigApplication.java
+++ src/main/java/com/springcloud/config/SpringCloudConfigApplication.java
@@ -3,19 +3,25 @@ package com.springcloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author forever_hgz
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudConfigApplication {
     public static void main(String[] args) {
-        // http://localhost:8080/redis/swagger-ui/index.html
-        // http://127.0.0.1:8080/redis/swagger-ui/index.html
-        // http://192.168.31.163:8080/redis/swagger-ui/index.html
-        // http://192.168.95.1:8080/redis/swagger-ui/index.html
-        // http://192.168.253.1:8080/redis/swagger-ui/index.html
+        // http://localhost:8081/springcloud-config-server/actuator/configserver
+        // http://127.0.0.1:8081/springcloud-config-server/actuator/configserver
+        // http://192.168.31.163:8081/springcloud-config-server/actuator/configserver
+        // http://192.168.95.1:8081/springcloud-config-server/actuator/configserver
+        // http://192.168.253.1:8081/springcloud-config-server/actuator/configserver
+        // http://127.0.0.1:8081/springcloud-config-server/actuator/configserver
+        // http://127.0.0.1:8081/springcloud-config-server/actuator/refresh
+        // http://127.0.0.1:8081/springcloud-config-server/secondconfig/dev/master
+        // http://127.0.0.1:8081/springcloud-config-server/secondconfig/refresh
         SpringApplication.run(SpringCloudConfigApplication.class, args);
     }
 
